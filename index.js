5e1bfce285018b41d092b19618367ebea45ae961
rhim <yeqiu.jinwoo@gmail.com>
2024-08-26 23:05:33 +0800

    third

--- index.js
+++ index.js
@@ -32,21 +32,21 @@ async function demoMain () {
         // #endregion
 
         // #region READ OPERATION
-        // const filter = { name: 'John Doe' };
-        // const cursor = await collection.findOne(filter);
-        // console.log(`\nAll documents:`);    
-        // cursor ? console.log(cursor) : console.log('No data has been stored in the database yet.');
+        const filter = { name: 'John Doe' };
+        const cursor = await collection.findOne(filter);
+        console.log(`\nAll documents:`);    
+        cursor ? console.log(cursor) : console.log('No data has been stored in the database yet.');
         // #endregion
         
         // #region UPDATE OPERATION
-        // const update = { $set: { age: 31 } };
-        // const updatedResult = await collection.updateOne(filter, update);
-        // console.log(`\nUpdated ${updatedResult.matchedCount} document(s).`);
+        const update = { $set: { age: 31 } };
+        const updatedResult = await collection.updateOne(filter, update);
+        console.log(`\nUpdated ${updatedResult.matchedCount} document(s).`);
         // #endregion
 
         // #region DELETE OPERATION
-        // const deletedResult = await collection.deleteOne(filter);
-        // console.log(`\nDeleted ${deletedResult.deletedCount} document(s).`);
+        const deletedResult = await collection.deleteOne(filter);
+        console.log(`\nDeleted ${deletedResult.deletedCount} document(s).`);
         // #endregion
     }
     catch (error) {
